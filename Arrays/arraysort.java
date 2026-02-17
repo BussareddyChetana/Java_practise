0519b25966d9046d3a6fee991510b44ab971ddcd
BussareddyChetana <bussareddychetana@gmail.com>
2026-02-17 21:34:15 +0530

    3rd

---
+++ Arrays/arraysort.java
@@ -0,0 +1,12 @@
+package Arrays;
+import java.util.*;
+public class arraysort { 
+  public static void main(String[] args) {
+      int[] arr={1,12,434,3639,23837};
+       Arrays.sort(arr);
+       for (int i = 0; i < arr.length; i++) {
+        System.out.println(arr[i]);
+       }
+  }
+}
+
