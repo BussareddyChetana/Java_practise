576aff97b09312374a5a94d5e9375160aab3b202
BussareddyChetana <bussareddychetana@gmail.com>
2026-02-17 20:58:40 +0530

    day1

---
+++ Arrays/Largest.java
@@ -0,0 +1,18 @@
+package Arrays;
+public  class Largest {
+
+  public static void main(String[] args) {
+    int[] arr = {10,20,30,40,50};
+    System.out.println(fun(arr));
+  }
+
+    private static int fun(int[] arr) {
+      int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+          if(max<arr[i]){
+            max = arr[i];
+          } 
+        }
+        return max;
+    }
+}
